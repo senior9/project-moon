edbd0484eb9195b03a2907688b57c81c1a60599e
senior9 <ovi215188@gmail.com>
2023-10-07 04:10:20 +0600

    project moon-kinght upcomming

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1 className='text-center text-amber-600 text-3xl uppercase'>Project upcomming Soon <br /> till  then have   a sweet dream</h1>
+    </div>
+  )
+}
+
+export default App
